ba9e788b0205e8532b325a6da36784d8d44042b9
Rafael Acioly <aciolyr@gmail.com>
2018-01-02 22:01:23 -0200

    comandos e variáveis

---
+++ section-3/comandos/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+// go build main.go
+// go run main.go
+// go vet main.go -- verifica erros de syntax
+func main() {
+	fmt.Printf("Outro programa em %s", "golang")
+}
