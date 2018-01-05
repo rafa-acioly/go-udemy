b94f2b8a72d81ce05db835fa66fc17f2825a978f
Rafael Acioly <aciolyr@gmail.com>
2018-01-05 21:50:28 -0200

    ifs

---
+++ controles/ifinit/ifinit.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func numeroAleatorio() int {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
+	return r.Intn(10)
+}
+
+func main() {
+	if i := numeroAleatorio(); i > 5 { // também suportado no switch
+		fmt.Println("Ganhou", i)
+	} else {
+		fmt.Println("Perdeu", i)
+	}
+}
